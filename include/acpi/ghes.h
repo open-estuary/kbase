fb876171ee5952f1ab02517c50876194f1996dee
Tyler Baicar <tbaicar@codeaurora.org>
2016-10-07 15:31:13 -0600

    acpi: apei: read ack upon ghes record consumption
    A RAS (Reliability, Availability, Serviceability) controller
    may be a separate processor running in parallel with OS
    execution, and may generate error records for consumption by
    the OS. If the RAS controller produces multiple error records,
    then they may be overwritten before the OS has consumed them.
    
    The Generic Hardware Error Source (GHES) v2 structure
    introduces the capability for the OS to acknowledge the
    consumption of the error record generated by the RAS
    controller. A RAS controller supporting GHESv2 shall wait for
    the acknowledgment before writing a new error record, thus
    eliminating the race condition.
    
    Signed-off-by: Jonathan (Zhixiong) Zhang <zjzhang@codeaurora.org>
    Signed-off-by: Richard Ruigrok <rruigrok@codeaurora.org>
    Signed-off-by: Tyler Baicar <tbaicar@codeaurora.org>
    Signed-off-by: Naveen Kaje <nkaje@codeaurora.org>

--- include/acpi/ghes.h
+++ include/acpi/ghes.h
@@ -14,6 +14,7 @@
 
 struct ghes {
 	struct acpi_hest_generic *generic;
+	struct acpi_hest_generic_v2 *generic_v2;
 	struct acpi_hest_generic_status *estatus;
 	u64 buffer_paddr;
 	unsigned long flags;
